0a08d132202843b3796f633630973df6c40d242a
IuriKiriak <best.iurik@gmail.com>
2024-02-19 14:34:31 +0300

    work press start

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const routeList = document.getElementById("route-list"); 
+const startTrip = document.getElementById("start-trip"); 
+const tripModal = document.getElementById("trip-modal"); 
+const tripModalCon = document.querySelector(".trip-modal-container"); 
+const closeTripModal = document.getElementById('close-trip-modal'); 
  
 const routes = []; 
  
-const getRoutes = async () => { 
-  await fetch("http://localhost:8080/routes") 
-    .then((response) => response.json()) 
-    .then((data) => { 
-      routes.push(...data); 
-      showRoutes(); 
-    }) 
-    .catch((error) => console.error(error)); 
+const getRoutes = () => { 
+  fetch("http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes", { 
+    headers: { 
+      Authorization: "30068957-8799-402b-bbcd-03a6a76f8f0a", 
+    }, 
+  }) 
 }; 
  
 const showRoutes = () => { 
@@ -33,5 +35,17 @@ const showRoutes = () => {
     routeList.append(oneRoute); 
   }); 
 }; 
+ 
+startTrip.onclick = (e) => { 
+  e.preventDefault(); 
+  tripModal.classList.remove("hide"); 
+  tripModalCon.classList.remove('hide'); 
+}; 
+ 
+closeTripModal.onclick = (e) => { 
+  e.preventDefault(); 
+  tripModal.classList.add("hide"); 
+  tripModalCon.classList.add('hide'); 
+} 
  
 getRoutes();
